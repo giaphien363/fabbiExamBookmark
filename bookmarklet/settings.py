5a5a7a71cec7225f0b232e3285e33538faab7328
giaphien363 <hienaloso98@gmail.com>
2021-11-29 17:05:15 +0700

    set up mongo db

--- bookmarklet/settings.py
+++ bookmarklet/settings.py
@@ -72,14 +72,28 @@ WSGI_APPLICATION = 'bookmarklet.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+DATABASES = {
+        'default': {
+            'ENGINE': 'djongo',
+            'NAME': 'Test',
+            'ENFORCE_SCHEMA': False,
+            'CLIENT': {
+                'host': 'localhost',
+                'port': 27017,
+                'username': 'admin',
+                'password': 'admin',
+            },
+        }
+    }
 
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+'''
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
--- readme.md
+++ readme.md
@@ -2,3 +2,27 @@
 
 ### Create venv
 `python -m venv venv`
+
+### Set up mongo in docker
+- `docker pull mongo`
+
+- `docker run --name mongo_docker  -p 27017:27017 -e MONGO_INITDB_ROOT_USERNAME=[admin] -e MONGO_INITDB_ROOT_PASSWORD=[admin] mongo`
+
+- `docker start mongo_docker`
+
+
+### Run container mongo_docker bash
+- `docker exec -it mongo_docker bash`
+- `mongo --username [admin] --password [admin]`
+- `use [db_name]`
+
+
+
+
+# Not fix yet
+### Create super user -- dont run this command
+`db.createUser({	
+    user: "admin",
+	pwd: "admin",
+	roles:[{role: "userAdminAnyDatabase" , db:"admin"}]
+})`
