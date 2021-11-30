650498a72ed2c4910eb4fb7246eb489c64a9a913
ThuGiangggggg <mai.thugiang2011@gmail.com>
2021-11-30 17:50:54 +0700

    validate category in bookmark

--- apps/bookmark/serializer.py
+++ apps/bookmark/serializer.py
@@ -1,7 +1,8 @@
 from apps.bookmark.models import BookMark
+from apps.category.models import Category
 from rest_framework import serializers
 
 class BookmarkSerializer(serializers.ModelSerializer) :
     class Meta :
         model = BookMark 
-        fields = '__all__'
+        fields = ['id', 'title', 'url', 'category']
--- apps/category/models.py
+++ apps/category/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class Category(models.Model):
-    categoryName = models.CharField(max_length=255, unique=True)
+    categoryName = models.CharField(max_length=255, unique=True, default='Bookmarks')
 
     def __str__(self):
         return self.categoryName
