9fa77a7fa918dd15e465db34dde1d10a8dea34db
ThuGiangggggg <mai.thugiang2011@gmail.com>
2021-12-07 14:14:14 +0700

    login and register FE

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,9 @@ import React, { Suspense } from "react";
 import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
 import Header from "./components/Header";
 import NotFound from "./components/NotFound";
+import Auth from "./features/Auth";
+import Login from "./features/Auth/components/Login";
+import Signup from "./features/Auth/components/Signup";
 import BookmarkLet from "./features/BookmarkLet";
 import Login from "./features/Auth/index";
 
@@ -15,8 +18,9 @@ function App() {
           <Header />
           <Switch>
             <Redirect exact from="/" to="/bookmark" />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/register" component={Signup} />
             <Route path="/bookmark" component={Bookmark} /> 
-            <Route path="/login" component={Login} /> 
             <Route path="/bookmarklet" component={BookmarkLet} /> 
             <Route component={NotFound} /> 
           </Switch> 
