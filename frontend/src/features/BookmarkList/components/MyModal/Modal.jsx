5c95787e90adee5870bf586ba12cd30fd528dbca
giaphien363 <hienaloso98@gmail.com>
2021-12-08 08:38:12 +0700

    add file modal dev

---
+++ frontend/src/features/BookmarkList/components/MyModal/Modal.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Modal } from "react-bootstrap";
+import AddEdit from "./AddEdit";
+
+const CustomModal = (prop) => {
+  const { showModal, toggleShow, groupId, bookId, listCategory, crudBookmark } =
+    prop;
+  return (
+    <>
+      <Modal
+        show={showModal}
+        backdrop="static"
+        keyboard={false}
+        onHide={toggleShow}
+      >
+        <Modal.Body>
+          <AddEdit
+            groupId={groupId}
+            bookId={bookId}
+            categoriesSelect={listCategory}
+            crudBookmark={crudBookmark}
+            toggleShow={toggleShow}
+          />
+        </Modal.Body>
+      </Modal>
+    </>
+  );
+};
+
+CustomModal.propTypes = {};
+
+export default CustomModal;
